ebfb2855afb39ed5c5c20d4a2fd78c9dffb64bf2
molotov.dmitry <molotov.dmitry@2c638967-16fe-4dfa-82d0-ae24e97dd189>
2016-04-03 12:43:07 +0000

    Merged revision(s) 28 from trunk: Request name moved from constructor to request call.
    ........

--- branches/vkapi_users/src/qvkrequest.cpp
+++ branches/vkapi_users/src/qvkrequest.cpp
@@ -5,29 +5,29 @@
 
 const QString VK_API_URL = QString("https://api.vk.com");
 
-QVkRequest::QVkRequest(const QByteArray &requestName,
-                       const QByteArray &token,
+QVkRequest::QVkRequest(const QByteArray &token,
                        QObject *parent) :
     QObject(parent),
     mApiVersion("5.50"),
-    mRequestName(requestName),
     mToken(token)
 {
     mManager = new QNetworkAccessManager(this);
     connect(mManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getReply(QNetworkReply*)));
 }
 
-void QVkRequest::sendRequest(const QParam &userParams)
+void QVkRequest::sendRequest(const QString &requestName,
+                             const QParam &userParams)
 {
-    QUrl requestUrl = prepareRequestUrl(userParams);
+    QUrl requestUrl = prepareRequestUrl(requestName, userParams);
 
     mManager->get(QNetworkRequest(requestUrl));
 }
 
-QUrl QVkRequest::prepareRequestUrl(const QParam &userParams)
+QUrl QVkRequest::prepareRequestUrl(const QString &requestName,
+                                   const QParam &userParams)
 {
     QString requestUrlString = QString("%1/method/%2.get").arg(VK_API_URL,
-                                                               QString(mRequestName));
+                                                               QString(requestName));
 
     QUrlQuery requestParam;
     requestParam.addQueryItem("access_token", mToken);
