ea7dd27c6eb1d22eaf7d9a39a6e1e8d3cd8bb540
molotov.dmitry <molotov.dmitry@2c638967-16fe-4dfa-82d0-ae24e97dd189>
2016-04-03 12:38:20 +0000

    Request name moved from constructor to request call.

--- trunk/include/qvkrequest.h
+++ trunk/include/qvkrequest.h
@@ -22,35 +22,36 @@ class QVkRequest : public QObject
 public:
     /*!
      * \brief Конструктор класса
-     * \param[in] requestName Название запроса
      * \param[in] token Идентификатор сессии
      * \param[in] parent Родительский объект
      */
-    explicit QVkRequest(const QByteArray &requestName,
-                        const QByteArray &token = QByteArray(),
+    explicit QVkRequest(const QByteArray &token = QByteArray(),
                         QObject *parent = 0);
 
     /*!
      * \brief Отправить запрос API Вконтакте
+     * \param[in] requestName Название запроса
      * \param[in] userParams Дополнительные параметры запроса
      */
-    void sendRequest(const QParam &userParams = QParam());
+    void sendRequest(const QString &requestName,
+                     const QParam &userParams = QParam());
 
 protected:
     QByteArray mApiVersion; ///< Используемая версия API ВКонтакте
 
 private:
-    QByteArray mRequestName;///< Название запроса
     QByteArray mToken;      ///< Идентификатор сессии
 
     QNetworkAccessManager *mManager; ///< Менеджер доступа к сети
 
     /*!
      * \brief Подготовка URL запроса
-     * \param userParams Дополнительные параметры запроса
+     * \param[in] requestName Название запроса
+     * \param[in] userParams Дополнительные параметры запроса
      * \return URL запроса
      */
-    QUrl prepareRequestUrl(const QParam &userParams);
+    QUrl prepareRequestUrl(const QString &requestName,
+                           const QParam &userParams);
 
 private slots:
 
