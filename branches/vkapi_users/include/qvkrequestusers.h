d8aac2c2f33f791fbbfb550c135ae25aa01f241a
molotov.dmitry <molotov.dmitry@2c638967-16fe-4dfa-82d0-ae24e97dd189>
2016-10-09 11:54:23 +0000

    Added counters user info structure; Added documentation for new/updated structures and methods; Added full user info request.

--- branches/vkapi_users/include/qvkrequestusers.h
+++ branches/vkapi_users/include/qvkrequestusers.h
@@ -2,31 +2,80 @@
   \file
   \brief Файл с описанием класса запросов информации о пользователях
 */
+
 #ifndef QVKREQUESTUSERS_H
 #define QVKREQUESTUSERS_H
 
 #include "qvkuserinfo.h"
 #include "qvkrequest.h"
 
+/*!
+ * \brief The QVkRequestUsers class
+ */
 class QVkRequestUsers : public QVkRequest
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief Конструктор класса
+     * \param[in] token Идентификатор сессии
+     * \param[in] parent Родительский объект
+     */
     explicit QVkRequestUsers(const QByteArray &token = QByteArray(),
                              QObject *parent = 0);
 
+    /*!
+     * \brief Запрос базовой информации о пользователе
+     * \param[in] userId Идентификатор пользователя
+     */
     void requestBasicUserInfo(unsigned long userId);
 
+    /*!
+     * \brief Запрос базовой информации о пользователе
+     * \param[in] userIds Перечисленные через запятую идентификаторы пользователей или их короткие имена
+     */
     void requestBasicUserInfo(QStringList userIds);
+
+    /*!
+     * \brief Запрос полной информации о пользователе
+     * \param[in] userId Идентификатор пользователя
+     */
+    void requestFullUserInfo(unsigned long userId);
+
+    /*!
+     * \brief Запрос полной информации о пользователе
+     * \param[in] userIds Перечисленные через запятую идентификаторы пользователей или их короткие имена
+     */
+    void requestFullUserInfo(QStringList userIds);
 
 private slots:
 
+    /*!
+     * \brief Получение ответа на запрос API ВКонтакте
+     * \param[in] document JSON-схема с ответом сервера
+     */
     void receiveBasicUserInfo(QJsonDocument document);
+
+    /*!
+     * \brief Получение ответа на запрос API ВКонтакте
+     * \param[in] document JSON-схема с ответом сервера
+     */
+    void receiveFullUserInfo(QJsonDocument document);
 
 signals:
 
+    /*!
+     * \brief Базовая информация о пользователе успешно получена
+     * \param[out] userInfoList Массив структур базовой информации о пользователях
+     */
     void basicUserInfoReceived(QList<VkUserInfoBasic> userInfoList);
+
+    /*!
+     * \brief Полная информация о пользователе успешно получена
+     * \param[out] userInfoList Массив структур полной информации о пользователях
+     */
+    void fullUserInfoReceived(QList<VkUserInfoFull> userInfoList);
 };
 
 #endif // QVKREQUESTUSERS_H
--- branches/vkapi_users/src/qvkrequestusers.cpp
+++ branches/vkapi_users/src/qvkrequestusers.cpp
@@ -4,11 +4,27 @@
 #include <QJsonObject>
 #include <QJsonValue>
 
+//// ___________________________________________________________________________
+//// ___________________________________________________________________________
+//// ___________________________________________________________________________
+
+//// ===========================================================================
+//// Конструктор ===============================================================
+//// ===========================================================================
+
 QVkRequestUsers::QVkRequestUsers(const QByteArray &token,
                                  QObject *parent) : QVkRequest(token, parent)
 {
 
 }
+
+//// ===========================================================================
+//// Полная информаци о пользователе ===========================================
+//// ===========================================================================
+
+//// ===========================================================================
+//// Запрос --------------------------------------------------------------------
+//// ===========================================================================
 
 void QVkRequestUsers::requestBasicUserInfo(unsigned long userId)
 {
@@ -32,6 +48,10 @@ void QVkRequestUsers::requestBasicUserInfo(QStringList userIds)
 
     sendRequest("users.get", paramList);
 }
+
+//// ===========================================================================
+//// Обработка ответа ----------------------------------------------------------
+//// ===========================================================================
 
 void QVkRequestUsers::receiveBasicUserInfo(QJsonDocument document)
 {
@@ -92,3 +112,227 @@ void QVkRequestUsers::receiveBasicUserInfo(QJsonDocument document)
     emit basicUserInfoReceived(userInfoList);
 }
 
+//// ===========================================================================
+//// Полная информаци о пользователе ===========================================
+//// ===========================================================================
+
+//// ===========================================================================
+//// Запрос --------------------------------------------------------------------
+//// ===========================================================================
+
+void QVkRequestUsers::requestFullUserInfo(unsigned long userId)
+{
+    QStringList userIds(QString::number(userId));
+
+    requestFullUserInfo(userIds);
+}
+
+void QVkRequestUsers::requestFullUserInfo(QStringList userIds)
+{
+    disconnect(this, 0, this, 0);
+
+    //// Список параметров =====================================================
+
+    QParam paramList;
+
+    //// Список идентификаторов пользователей ----------------------------------
+
+    QString usersList = userIds.join(',');
+
+    paramList.insert("user_ids", usersList);
+
+    //// Список полей ----------------------------------------------------------
+
+    QStringList fields;
+    fields << "bdate" << "sex" << "online" << "status" << "last_seen" << "verified" << "city" << "country" << "home_town" << "contacts" << "counters";
+
+    QString fieldsList = fields.join(',');
+
+    paramList.insert("fields", fieldsList);
+
+    //// =======================================================================
+
+    connect(this, SIGNAL(replySuccess(QJsonDocument)),
+            this, SLOT(receiveFullUserInfo(QJsonDocument)));
+
+    sendRequest("users.get", paramList);
+}
+
+//// ===========================================================================
+//// Обработка ответа ----------------------------------------------------------
+//// ===========================================================================
+
+void QVkRequestUsers::receiveFullUserInfo(QJsonDocument document)
+{
+    QList<VkUserInfoFull> userInfoList;
+
+    QJsonArray response = document.object()["response"].toArray();
+
+    foreach (const QJsonValue &value, response)
+    {
+        QJsonObject object = value.toObject();
+        VkUserInfoFull userInfo;
+
+        //// Базовая информаци о пользователе ==================================
+
+        //// Get user id and name ----------------------------------------------
+
+        userInfo.basic.id = object["id"].toVariant().toUInt();
+        userInfo.basic.firstName = object["first_name"].toString();
+        userInfo.basic.lastName = object["last_name"].toString();
+
+        //// Get page status ---------------------------------------------------
+
+        if (object.contains("deactivated"))
+        {
+            QString pageStatus = object["deactivated"].toString();
+
+            if (pageStatus == QString("deleted"))
+                userInfo.basic.pageStatus = VkUser::USER_DELETED;
+            else if (pageStatus == QString("banned"))
+                userInfo.basic.pageStatus = VkUser::USER_BANNED;
+            else
+                userInfo.basic.pageStatus = VkUser::USER_ACTIVE;
+        }
+        else
+        {
+            userInfo.basic.pageStatus = VkUser::USER_ACTIVE;
+        }
+
+        //// Get user visibility -----------------------------------------------
+
+        userInfo.basic.userVisibility = VkUser::USER_VISIBLE;
+
+        if (object.contains("hidden"))
+        {
+            int userHidden= object["hidden"].toInt();
+
+            if (userHidden)
+                userInfo.basic.userVisibility = VkUser::USER_HIDDEN;
+        }
+
+        if (userInfo.basic.pageStatus != VkUser::USER_ACTIVE || userInfo.basic.userVisibility != VkUser::USER_VISIBLE)
+        {
+            userInfoList.append(userInfo);
+            continue;
+        }
+
+        //// Информаци о статусе пользователя ==================================
+
+        //// Дата рождения -----------------------------------------------------
+
+        QString bDate = object["bdate"].toString();
+        QStringList bDateList = bDate.split('.');
+
+        if (bDateList.count() > 0)
+            userInfo.status.birthDay = bDateList.at(0).toInt();
+
+        if (bDateList.count() > 1)
+            userInfo.status.birthMonth = bDateList.at(1).toInt();
+
+        if (bDateList.count() > 2)
+            userInfo.status.birthYear = bDateList.at(2).toInt();
+
+        //// Пол пользователя --------------------------------------------------
+
+        userInfo.status.userSex = (VkUser::Sex)object["sex"].toInt();
+
+        //// Онлайн пользователя -----------------------------------------------
+
+        userInfo.status.userOnline = (VkUser::Online)object["online"].toInt();
+
+        if (userInfo.status.userOnline != VkUser::USER_OFFLINE)
+        {
+            if (object.contains("online_mobile") && object["online_mobile"].toInt() != 0)
+                userInfo.status.userOnline = VkUser::USER_ONLINE_MOBILE;
+        }
+
+        //// Статус пользователя -----------------------------------------------
+
+        userInfo.status.statusText = object["status"].toString();
+
+        //// Время последнего посещения ----------------------------------------
+
+        if (object.contains("last_seen"))
+        {
+            QJsonObject lastSeenObject = object["last_seen"].toObject();
+
+            userInfo.status.lastSeen = QDateTime::fromTime_t(lastSeenObject["time"].toVariant().toULongLong());
+        }
+
+        //// Сраница верифицирована --------------------------------------------
+
+        userInfo.status.verified = object["verified"].toInt() != 0;
+
+        //// Информация о контактах пользователя ===============================
+
+        //// Город -------------------------------------------------------------
+
+        if (object.contains("city"))
+        {
+            QJsonObject cityObject = object["city"].toObject();
+
+            userInfo.contacts.city = cityObject["title"].toString();
+        }
+
+        //// Страна ------------------------------------------------------------
+
+        if (object.contains("country"))
+        {
+            QJsonObject countryObject = object["country"].toObject();
+
+            userInfo.contacts.country = countryObject["title"].toString();
+        }
+
+        //// Родной город пользователя -----------------------------------------
+
+        if (object.contains("home_town"))
+        {
+            userInfo.contacts.homeTown = object["home_town"].toString();
+        }
+
+        //// Номер мобильного телефона -----------------------------------------
+
+        if (object.contains("mobile_phone"))
+            userInfo.contacts.mobilePhone = object["mobile_phone"].toString();
+
+
+        //// Дополнительный номер телефона -------------------------------------
+
+        if (object.contains("home_phone"))
+            userInfo.contacts.homePhone = object["home_phone"].toString();
+
+        //// Счётчики ==========================================================
+
+        if (object.contains("counters"))
+        {
+            QJsonObject countersObject = object["counters"].toObject();
+
+            userInfo.counters.albums = countersObject["albums"].toVariant().toUInt();
+            userInfo.counters.videos = countersObject["videos"].toVariant().toUInt();
+            userInfo.counters.audios = countersObject["audios"].toVariant().toUInt();
+            userInfo.counters.notes = countersObject["notes"].toVariant().toUInt();
+            userInfo.counters.photos = countersObject["photos"].toVariant().toUInt();
+            userInfo.counters.groups = countersObject["groups"].toVariant().toUInt();
+            userInfo.counters.gifts = countersObject["gifts"].toVariant().toUInt();
+            userInfo.counters.friends = countersObject["friends"].toVariant().toUInt();
+            userInfo.counters.online_friends = countersObject["online_friends"].toVariant().toUInt();
+            userInfo.counters.mutual_friends = countersObject["mutual_friends"].toVariant().toUInt();
+            userInfo.counters.user_photos = countersObject["user_photos"].toVariant().toUInt();
+            userInfo.counters.user_videos = countersObject["user_videos"].toVariant().toUInt();
+            userInfo.counters.followers = countersObject["followers"].toVariant().toUInt();
+            userInfo.counters.subscriptions = countersObject["subscriptions"].toVariant().toUInt();
+            userInfo.counters.pages = countersObject["pages"].toVariant().toUInt();
+        }
+        else
+        {
+            memset(&userInfo.counters, 0x00, sizeof(userInfo.counters));
+        }
+
+        //// Add user info to list =============================================
+
+        userInfoList.append(userInfo);
+    }
+
+    emit fullUserInfoReceived(userInfoList);
+}
