791c69f181fcc9bb466419c1f107c8f216978049
molotov.dmitry <molotov.dmitry@2c638967-16fe-4dfa-82d0-ae24e97dd189>
2016-11-01 16:29:49 +0000

    Merged revision(s) 82 from trunk: Added settings dialog button to main window. ........

--- branches/vkapi_photos/test/mainwindow.cpp
+++ branches/vkapi_photos/test/mainwindow.cpp
@@ -6,6 +6,8 @@
 
 #include <QPainter>
 
+#include "dialogsettings.h"
+
 #include "settings.h"
 #include "metadata.h"
 
@@ -32,6 +34,7 @@ MainWindow::MainWindow(const AccountInfo &accInfo, QWidget *parent) :
 
     setIcon(ui->buttonLogout, "user-logout.svg");
     setIcon(ui->buttonSwitchUser, "user-switch.svg");
+    setIcon(ui->buttonSettings, "configure.svg");
     setIcon(ui->buttonUpdate, "refresh.svg");
     setIcon(ui->buttonOpenLink, "open-link.svg");
 
@@ -342,4 +345,10 @@ void MainWindow::openPage(const QString &pageUri)
         showError("Unknown page uri: " + pageUri);
         break;
     }
+}
+
+void MainWindow::on_buttonSettings_clicked()
+{
+    DialogSettings settings;
+    settings.exec();
 }
